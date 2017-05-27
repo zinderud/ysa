5ba1307afb43ec440410eb5e367731cfed4bbf18
zinderud <mokosam@gmail.com>
2017-05-27 03:14:16 +0300

    as

--- Arsa_tahmin.py
+++ Arsa_tahmin.py
@@ -1,5 +1,10 @@
+# 
+#  Arsa_tahmin.py
+#
+#  ysa uygulama örneği
 #  Arsa tahmini basit bir ysa
-#  import tensorflow as tf
+
+import tensorflow as tf
 import numpy as np
 import math
 import matplotlib.pyplot as plt
@@ -18,3 +23,29 @@ plt.plot(arsa_size,arsa_price,"bx")
 plt.ylabel("price")
 plt.xlabel("size")
 plt.show();
+
+#verileri normalize edecek fonk
+def normalize(array):
+    return(array-array.mean())/array.std()
+
+# eğitim dataları %70 alalım
+num_egitim_ornek=math.floor(num_arsa*0.7z)
+
+# egitim dataları tanımlayalım
+egitim_arsa_size=np.asarray(arsa_size[:num_egitim_ornek])
+eitim_arsa_price=np.asanyarray(arsa_price[:num_egitim_ornek:])
+
+#normalize edelim
+egitim_arsa_size_norm=normalize(egitim_arsa_size)
+eitim_arsa_price_norm=normalize(eitim_arsa_price)
+
+#test dataları tanımlayalım
+test_arsa_size=np.array(arsa_size[num_egitim_ornek:])
+test_arsa_price=np.array(arsa_price[num_egitim_ornek:])
+
+#normalize edelim
+test_arsa_size_norm=normalize(test_arsa_size)
+test_arsa_price_norm=normalize(test_arsa_price)
+
+
+
