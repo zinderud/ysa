a2bf9e7e71285753567798094b9631d24518e0bc
zinderud <mokosam@gmail.com>
2017-06-02 02:31:36 +0300

    dt

---
+++ sklearn/breast-cancer.py
@@ -0,0 +1,30 @@
+
+import numpy as np
+from sklearn.preprocessing import Imputer
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+import pandas as pd
+
+veri = pd.read_csv("data.csv")
+
+veri=veri.replace('?', -222, inplace='true')
+veri=veri.drop(['id'], axis=1)
+
+
+y = veri.benormal
+x = veri.drop(['benormal'], axis=1)
+
+imp = Imputer(missing_values=-222, strategy="mean",axis=0)
+x = imp.fit_transform(x)
+
+ 
+
+
+tahmin = KNeighborsClassifier(n_neighbors=4, weights='uniform', algorithm='auto', leaf_size=30, p=2, metric='euclidean', metric_params=None, n_jobs=1)
+tahmin.fit(x,y)
+ytahmin = tahmin.predict(x)
+
+basari = accuracy_score(y, ytahmin, normalize=True, sample_weight=None)
+print("Yüzde",basari*100," oranında:" )
+
+print(tahmin.predict([1,1,1,1,1,1,1,1,1,1]))
