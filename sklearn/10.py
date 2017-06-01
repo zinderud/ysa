9ead53ff5741a42887cda25a6b97f6838d753293
zinderud <mokosam@gmail.com>
2017-06-01 16:57:11 +0300

    random

---
+++ sklearn/10.py
@@ -0,0 +1,37 @@
+
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+ 
+# Create some simple data
+ 
+np.random.seed(0)
+X = np.random.random(size=(20, 1))
+y = 3 * X.squeeze() + 2 + np.random.randn(20)
+
+plt.plot(X.squeeze(), y, 'o');
+
+
+model = LinearRegression()
+model.fit(X, y)
+
+# Plot the data and the model prediction
+X_fit = np.linspace(0, 1, 100)[:, np.newaxis]
+y_fit = model.predict(X_fit)
+
+plt.plot(X.squeeze(), y, 'o')
+plt.plot(X_fit.squeeze(), y_fit);
+plt.show()
+
+# Fit a Random Forest
+from sklearn.ensemble import RandomForestRegressor
+model = RandomForestRegressor()
+model.fit(X, y)
+
+# Plot the data and the model prediction
+X_fit = np.linspace(0, 1, 100)[:, np.newaxis]
+y_fit = model.predict(X_fit)
+
+plt.plot(X.squeeze(), y, 'o')
+plt.plot(X_fit.squeeze(), y_fit);
