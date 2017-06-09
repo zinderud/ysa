e98a5e8036afa7fd1ea9f7186b5e4225e35e3d1c
zinderud <mokosam@gmail.com>
2017-06-09 21:29:18 +0300

    e

--- python/Patterns/abstraction/main.py
+++ python/Patterns/abstraction/main.py
@@ -1,10 +1,16 @@
-class Rectangle(object):
+class Dortgen(object):
 
     @staticmethod
-    def calculate_area(width, height):
+    def Alan_Hesapla(width, height):
         return width * height
+    
+    @staticmethod
+    def Cevre_hesapla(width, height):
+        return 2*(width+height)
 
 
 # User does not need to know formula to get area of Rectangle
-area = Rectangle.calculate_area(5, 7)
-print(area)
+alan = Dortgen.Alan_Hesapla(5, 7)
+print(alan)
+cevre=Dortgen.Cevre_hesapla(5,7)
+print(cevre)
