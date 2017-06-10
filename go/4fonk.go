223a93e8b42a8c2af86e41cc6147304293d26081
zinderud <mokosam@gmail.com>
2017-06-10 18:32:12 +0300

    ok

--- go/4fonk.go
+++ go/4fonk.go
@@ -10,3 +10,29 @@ func main(){
 func Topla(x int,y int) int{
 	return x+y
 }
+
+
+//recursive fonk örneği
+func Faktoriyel(sayi int) int{
+	if sayii==0 || sayi==1{
+		return 1
+	} else {
+		return sayi*Faktoriyel(sayi-1)
+	}
+}
+
+func seriIstek (x,y float32)(topla,bol float32)
+	Topla =x+y
+	bol=x/y
+	return topla,bol
+
+//n sayida parametre gönderebiliriz
+func toplama (sayilar ...int)int{
+	toplam:=0
+
+
+	for _,sayi:=range sayilar {
+		toplam+=sayilar
+	}
+	return toplam
+}
